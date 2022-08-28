930408f40ea20fa8873939e616e262f829814564
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-08-28 01:00:21 -0400

    removed test from server index.js deployement is not happening properly yet

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import path from 'path';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { fetchGeo } from './utils/fetchGeo.js';
@@ -9,14 +8,13 @@ dotenv.config();
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(cors());
 app.use(express.json({ extended: false }));
 
 // from create react app doc for deployement
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/', (req, res) => {
+  res.json('hello server');
 });
 
 // Get lat lon for a givin city
